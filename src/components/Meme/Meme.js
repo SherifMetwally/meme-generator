48ba55b3030f5f6b78dd1619370153700837af34
sherifmetwally <sherifmetwally@hotmail.com>
2022-09-19 15:42:38 +0200

    Final

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -1,31 +1,73 @@
-import  {useState} from 'react';
-import "./meme.css";
-import memesData  from "./memesData"; 
+import React from "react"
+import memesData from "./memesData.js"
 
 export default function Meme() {
-
-  const [image , setImage] = useState('')
-
-    function getMemeImage() {
-    let memesArray = memesData.data.memes
-    const meme = memesArray[Math.floor(Math.random() * memesArray.length)]
-    const {url} =  meme
-    console.log(meme)
-    console.log(url)
-    setImage(url)
-}
-
-  return (
-    <main>
-    <div className="form">
-      <input type="text" placeholder="Top Text" className="input-field" />
-      <input type="text" placeholder="Bottm Text" className="input-field" />
-      <button className="input-button" onClick={getMemeImage}>Get a new meme image</button>
-    </div>
-    <div  class="meme-image">
-    <img src={image} alt="meme"/>
-    </div>
+    /**
+     * Challenge: 
+     * 1. Set up the text inputs to save to
+     *    the `topText` and `bottomText` state variables.
+     * 2. Replace the hard-coded text on the image with
+     *    the text being saved to state.
+     */
     
-    </main>
-  );
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    
+    function getMemeImage() {
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+        
+    }
+    
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    
+    return (
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={meme.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={meme.bottomText}
+                    onChange={handleChange}
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <div className="meme">
+                <img src={meme.randomImage} className="meme--image" />
+                <h2 className="meme--text top">{meme.topText}</h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+            </div>
+        </main>
+    )
 }
